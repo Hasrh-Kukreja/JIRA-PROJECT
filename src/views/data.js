3eecf1a1a3345d00dd6545deaef2f91d0bb17d94
Harsh-Kukreja <Hunnykukreja05@gmail.com>
2023-01-10 20:58:30 +0530

    Modified the attribute name

--- src/views/data.js
+++ src/views/data.js
@@ -2,7 +2,7 @@ const tickets = [{
     Id: 101,
     Name: "Create table to show all tickets",
     Description: "You have to create a table to show all the tickets for that particular Jira account",
-    Reporter: "Saurabh Saini",
+    Reporter: "Harsh Kukreja",
     Status: "In Progress",
     Due_Date: "31-12-2022"
 }];
--- src/views/ticketsTable.jsx
+++ src/views/ticketsTable.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Select from "react-select";
-// const { tickets } = require("./data");
 const statusOptions = [
   { label: "To Do", value: "11" },
   { label: "In Progress", value: "21" },
@@ -10,7 +9,6 @@ const statusOptions = [
 
 export default function TableTickets(props) {
   let {tickets , setAllTickets} = props
-  // let [allTickets,setTickets] = useState(tickets)
   const handleTicketStatus = (e,index) => {
     console.log("EVENT AND INDEX", e, index)
     let tempAllTickets = [...tickets];
@@ -31,8 +29,8 @@ export default function TableTickets(props) {
       
         {allTickets?.map((ticket,index) => (
          <div className="row" key={index}>
-            <div className="col-2 border py-2">{ticket.Id}</div>
-            <div className="col-2 border py-2">{ticket.Name}</div>
+            <div className="col-2 border py-2">{ticket.id}</div>
+            <div className="col-2 border py-2">{ticket.name}</div>
             <div className="col-2 border py-2">{ticket.Description}</div>
             <div className="col-2 border py-2">{ticket.Reporter}</div>
             <div className="col-2 border py-2">
